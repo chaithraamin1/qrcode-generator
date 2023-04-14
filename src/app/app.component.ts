b1fdf7e25e842aa21b385b95a548075b50147e05
chai <chaithra.amin98@gmail.com>
2023-04-14 14:23:39 +0530

    qrcode-generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'qrcode-generator';
+  ngOnInit(){
+
+  }
+  generateQR(){
+    debugger
+    var inputText:any=document.getElementById("input-text");
+    var qrImg:any=document.getElementById("qrImg");
+    var imgBox:any=document.getElementById("imgBox");
+    if(inputText.value.length>0){
+   
+    var qr_code="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+inputText.value;
+    qrImg.src=qr_code;
+    imgBox.classList.add("show-img");
+    }
+    else{
+      imgBox.classList.remove("show-img");
+      inputText.classList.add("error");
+      setTimeout(()=>{
+        inputText.classList.remove("error");
+      })
+    }
+
+
+  }
 }
